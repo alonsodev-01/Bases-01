79c82193b4389970e2352bdaad7a436ac00b12dd
alonsodev-01 <alonsodev.ga@gmail.com>
2026-06-02 16:07:49 -0500

    Ejercicio 9

---
+++ src/exercises4/Practice9.java
@@ -0,0 +1,34 @@
+package exercises4;
+
+public class Practice9 {
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println("Matriz original");
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
+        };
+
+        System.out.println("Matriz transpuesta");
+        int[][] matrizTranspuesta = new int[matriz.length][matriz[0].length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matrizTranspuesta[i][j] = matriz[j][i];
+            }
+        }
+
+        for (int i = 0; i < matrizTranspuesta.length; i++) {
+            for (int j = 0; j < matrizTranspuesta[i].length; j++) {
+                System.out.print(matrizTranspuesta[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
